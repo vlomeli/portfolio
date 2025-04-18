e674b2cee821c2490fb2ebd885cfa533b86a46be
Victor Lomeli <victorlomeli2023@gmail.com>
2025-04-18 12:26:32 -0700

    new repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+// import navbar 
+// import about me 
+// import projects 
+// import footer
+
+function App() {
+  return (
+      <>
+      
+      </>
+  )
+}
+
+export default App
