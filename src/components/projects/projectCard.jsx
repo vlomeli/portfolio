f5049e453a6062e92cb3070607ed8038821d1bdf
Victor Lomeli <victorlomeli2023@gmail.com>
2025-04-21 14:35:50 -0700

    added animation to projects

--- src/components/projects/projectCard.jsx
+++ src/components/projects/projectCard.jsx
@@ -1,25 +1,46 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { Star } from "lucide-react";
 import "./ProjectCard.css";
 
-const ProjectCard = ({ img, title, description, githubURL, isTopProject }) => {
+const cardVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: i => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.3, // domino effect
+        duration: 0.3,
+        ease: "easeOut",
+      },
+    }),
+  };
+  
+  function ProjectCard ({ img, title, description, githubURL, isTopProject, index }) {
     return (
-        <div className="project-card">
-          <img src={img} alt={title} className="project-img" />
-          <div className="project-content">
-            <div className="project-header">
-              <h3>{title}</h3>
-              {isTopProject && <Star size={18} className="star-icon" />}
-            </div>
-            <p>{description}</p>
-            {githubURL && (
-              <a href={githubURL} target="_blank" rel="noopener noreferrer">
-                <button className="project-btn">View Project</button>
-              </a>
-            )}
+      <motion.div
+        className="project-card"
+        custom={index} // use index for delay
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: false, amount: 0.5 }}
+        variants={cardVariants}
+      >
+        <img src={img} alt={title} className="project-img" />
+        <div className="project-content">
+          <div className="project-header">
+            <h3>{title}</h3>
+            {isTopProject && <Star className="star-icon" fill="#facc15" />}
           </div>
+          <p>{description}</p>
+          {githubURL && (
+            <a href={githubURL} target="_blank" rel="noopener noreferrer">
+              <button className="project-btn">View Code</button>
+            </a>
+          )}
         </div>
-      );
-    };
-    
-    export default ProjectCard;
+      </motion.div>
+    );
+  };
+  
+  export default ProjectCard;
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -16,7 +16,7 @@ return (
     <h2 style={{ textAlign: "center", marginTop: "2rem" }}>Projects</h2>
     <div className="projects-grid">
       {combinedProjects.map((project, index) => (
-        <ProjectCard key={index} {...project} />
+        <ProjectCard key={index} {...project} index={index} />
       ))}
     </div>
   </section>
